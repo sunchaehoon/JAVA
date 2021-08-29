440af374aa44bb23adcc9397594d9acb026418bb
선채훈 <80810317+sunchaehoon@users.noreply.github.com>
2021-08-29 12:41:43 +0900

    Create java9.java

---
+++ java9.java
@@ -0,0 +1,15 @@
+package chap02;
+
+public class chap02_3 {
+    public static void main(String[] args) {
+        //논리 부정 연산자
+        boolean play = true;
+        System.out.println(play);
+
+        play = !play;
+        System.out.println(play);
+
+        play = !play;
+        System.out.println(play);
+    }
+}
