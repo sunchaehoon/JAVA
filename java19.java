94361c9ee7101aa7aed2c499e8fc3e2d3d82270b
선채훈 <80810317+sunchaehoon@users.noreply.github.com>
2021-08-30 23:07:33 +0900

    Create java19.java

---
+++ java19.java
@@ -0,0 +1,23 @@
+package chap04;
+
+public class chap04_3 {
+    public static void main(String[] args) {
+        //값 목록으로 배열 생성
+        int[] scores;
+        scores = new int[] {83, 90, 87};
+        int sum1 = 0;
+        for (int i=0;i<3;i++)
+            sum1 += scores[i];
+        System.out.println("총합 : " + sum1);
+
+        int sum2 = add( new int[] {83, 90, 87});
+        System.out.println("총합 : " + sum2);
+        System.out.println();
+    }
+    public static int add(int[] scores){
+        int sum = 0;
+        for (int i=0;i<3;i++)
+            sum += scores[i];
+        return sum;
+    }
+}
