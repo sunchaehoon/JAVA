d3ad7d7a34cb107a41840f2c5404d446ff5e2219
선채훈 <80810317+sunchaehoon@users.noreply.github.com>
2021-09-01 23:45:21 +0900

    Create java30.java

---
+++ java30.java
@@ -0,0 +1,15 @@
+package chap05;
+
+public class chap05_1 {
+    //생성자에서 필드 초기화
+    //필드
+    String nation = "대한민국";
+    String name;    //name --> 필드
+    String ssn;    //ssn --> 필드
+
+    //생성자
+    public chap05_1(String n, String s) {
+        name = n;
+        ssn = s;
+    }
+}
