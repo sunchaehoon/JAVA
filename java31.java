5b00ce7b2f06364bae1f51aa700aa25e0e8ef869
선채훈 <80810317+sunchaehoon@users.noreply.github.com>
2021-09-02 11:42:39 +0900

    Create java31.java

---
+++ java31.java
@@ -0,0 +1,15 @@
+package chap05;
+
+public class Korean {
+    //생성자에서 필드 초기화
+    //필드
+    String nation = "대한민국";
+    String name;
+    String ssn;
+
+    //생성자
+    public Korean(String n, String s){
+        name = n;
+        ssn = s;
+    }
+}
