954f1ecef34a94517dec3dbad2b113d0d2e8870d
선채훈 <80810317+sunchaehoon@users.noreply.github.com>
2021-09-08 20:53:47 +0900

    Create java49.java

---
+++ java49.java
@@ -0,0 +1,10 @@
+package chap06;
+
+public class CalculatorExample {
+    public static void main(String[] args) {
+        //Calculator의 execute() 실행
+
+        Calculator myCalc = new Calculator();
+        myCalc.execute();
+    }
+}
