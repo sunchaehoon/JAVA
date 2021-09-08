9a1dcdbf86b7ab73009c258ef5108a80895602f6
선채훈 <80810317+sunchaehoon@users.noreply.github.com>
2021-09-08 21:15:16 +0900

    Update java52.java

--- java52.java
+++ java52.java
@@ -1,17 +1,13 @@
 package chap06;
 
-public class CalculatorExample2 {
-    public static void main(String[] args) {
-        Calculator2 myCalcu = new Calculator2();
+public class Calculator2 {
+    //정사각형의 넓이
+    double areaRectangle(double width) {
+        return width * width;
+    }
 
-        //정사각형의 넓이 구하기
-        double result1 = myCalcu.areaRectangle(10);
-
-        //직사각형의 넓이 구하기
-        double result2 = myCalcu.areaRectangle(10, 20);
-
-        //결과 출력
-        System.out.println("정사각형 넓이=" + result1);
-        System.out.println("직사각형 넓이=" + result2);
+    //직사각형의 넓이
+    double areaRectangle(double width, double height) {
+        return width * height;
     }
 }
