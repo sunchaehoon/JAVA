2387db96caa605a0034ae6a8490a24db790ec017
선채훈 <80810317+sunchaehoon@users.noreply.github.com>
2021-09-08 20:52:59 +0900

    Create java48.java

---
+++ java48.java
@@ -0,0 +1,25 @@
+package chap06;
+
+public class Calculator {
+    //클래스 내부에서 메소드 호출
+
+    int plus(int x, int y) {
+        int result = x + y;
+        return result;
+    }
+
+    double avg(int x, int y) {
+        double sum = plus(x ,y);
+        double result = sum / 2;
+        return result;
+    }
+
+    void execute() {
+        double result = avg(7, 10);
+        println("실행 결과: " + result);
+    }
+
+    void println(String message) {
+        System.out.println(message);
+    }
+}
