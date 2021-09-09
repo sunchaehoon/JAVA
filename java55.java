8562a495dcc776bd6f7e9e3fb737543d2e9a0f1b
선채훈 <80810317+sunchaehoon@users.noreply.github.com>
2021-09-09 17:50:14 +0900

    Create java55.java

---
+++ java55.java
@@ -0,0 +1,13 @@
+package chap04;
+
+public class human {
+    String name;
+    double height;
+
+    void dancing() {
+        System.out.println("키가 " + height + "인 " + name + "이(가) 춤을 춘다.");
+    }
+    void singing() {
+        System.out.println("키가 " + height + "인 " + name + "이(가) 노래를 한다.");
+    }
+}
