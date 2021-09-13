1a59567b472e3b58f6cb5f81132dbe5db1f6b415
선채훈 <80810317+sunchaehoon@users.noreply.github.com>
2021-09-13 16:07:40 +0900

    Create java74.java

---
+++ java74.java
@@ -0,0 +1,20 @@
+package problem2;
+
+class Printer {
+    int numOfPapers = 0;
+
+    public Printer(int numOfPapers) {
+        this.numOfPapers += numOfPapers;
+    }
+
+    void print(int amount) {    //amount = 출력하는 양
+        numOfPapers -= amount;
+        System.out.println(numOfPapers);
+    }
+}
+    public class ex01 {
+        public static void main(String[] args) {
+            Printer p = new Printer(100);
+            p.print(70);
+        }
+    }
