606617ad9477b4f942e3164ef62e85b4ca0df21c
선채훈 <80810317+sunchaehoon@users.noreply.github.com>
2021-09-15 15:36:43 +0900

    Create java85.java

---
+++ java85.java
@@ -0,0 +1,11 @@
+package chap06;
+
+public class CarExample3 {
+    public static void main(String[] args) {
+        Car3 myCar = new Car3("포르쉐");
+        Car3 yourCar = new Car3("벤츠");
+
+        myCar.run();
+        yourCar.run();
+    }
+}
