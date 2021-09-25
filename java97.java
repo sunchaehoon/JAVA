7ff30c4d763b8ee1839678c84ff978dafd0b21f9
선채훈 <80810317+sunchaehoon@users.noreply.github.com>
2021-09-26 02:19:44 +0900

    Create java97.java

---
+++ java97.java
@@ -0,0 +1,24 @@
+package problem2;
+
+class Car {
+    String color;
+    static int getNumOfCar;
+    static int getNumOfRedCar;
+
+    public Car(String color) {
+        this.color = color;
+        getNumOfCar++;
+        if (color.equals("red") | color.equals("RED"))
+            getNumOfRedCar++;
+    }
+}
+
+public class CarTest {
+    public static void main(String[] args) {
+        Car c1 = new Car("red");
+        Car c2 = new Car("blue");
+        Car c3 = new Car("RED");
+
+        System.out.printf("자동차 수 : %d, 빨간색 자동차 수 : %d", Car.getNumOfCar, Car.getNumOfRedCar);
+    }
+}
