f40d3ea7acf7ec9201e599648b202c8eb00134b9
선채훈 <80810317+sunchaehoon@users.noreply.github.com>
2021-09-27 15:18:03 +0900

    Create java108.java

---
+++ java108.java
@@ -0,0 +1,15 @@
+package chap06;
+
+public class Student extends People {
+    int hakbun;
+
+    public Student(String name, String ssn, int hakbun) {
+        super(name, ssn);
+        this.hakbun = hakbun;
+    }
+
+    // 메서드 오버라이딩
+    void fall() {
+        System.out.println("나는 사람이면서 학생입니다.");
+    }
+}
