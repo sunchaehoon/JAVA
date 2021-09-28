cb1cd80f5a3de10c90dc71fab33e27448a22133c
선채훈 <80810317+sunchaehoon@users.noreply.github.com>
2021-09-28 23:12:32 +0900

    Create java115.java

---
+++ java115.java
@@ -0,0 +1,9 @@
+package chap07;
+
+public class EarthExmple {
+    //상수 사용
+    public static void main(String[] args) {
+        System.out.println("지구의 반지름 : " + Earth.EARTH_RADIUS + "km");
+        System.out.println("지구의 표면적 : " + Earth.EARTH_AREA + "km^2");
+    }
+}
