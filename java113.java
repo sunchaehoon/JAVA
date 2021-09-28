1a753e338c11bea5c6402a72f7d38be8577d7672
선채훈 <80810317+sunchaehoon@users.noreply.github.com>
2021-09-28 23:12:11 +0900

    Create java113.java

---
+++ java113.java
@@ -0,0 +1,8 @@
+package chap07;
+
+public class Earth {
+    // 상수 --> 일반적으로 불변의 값(static final)
+    // 상수 선언
+    static final double EARTH_RADIUS = 6400;
+    static final double EARTH_AREA = 4 * Math.PI * EARTH_RADIUS * EARTH_RADIUS;
+}
