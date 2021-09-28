0549a9647629a2163283ee4190d9b70b99e9d39c
선채훈 <80810317+sunchaehoon@users.noreply.github.com>
2021-09-28 23:11:27 +0900

    Create java110.java

---
+++ java110.java
@@ -0,0 +1,14 @@
+package chap07;
+
+public class Calculator {
+    //정적 멤버 사용
+    static double pi = 3.14159;
+
+    static int plus(int x, int y) {
+        return x + y;
+    }
+
+    static int minus(int x, int y) {
+        return x - y;
+    }
+}
