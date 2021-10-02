8522182ef6a96bca558708bbbd350d46e1266683
선채훈 <80810317+sunchaehoon@users.noreply.github.com>
2021-10-02 20:10:22 +0900

    Create java129.java

---
+++ java129.java
@@ -0,0 +1,19 @@
+package chap07;
+
+public class IncrementDemo {
+    // 메서드의 인수로 배열 전달
+    public static void main(String[] args) {
+        int[] x = { 0 };
+        System.out.println("호출 전의 x[0] = " + x[0]);
+
+        increment(x);
+        System.out.println("호출 후의 x[0] = " + x[0]);
+    }
+
+    public static void increment(int[] n) {
+        System.out.print("increment() 메서드 안에서 ");
+        System.out.print("n[0] = " + n[0] + " ---> ");
+        n[0]++;
+        System.out.println("n[0] = " + n[0]);
+    }
+}
