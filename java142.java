7af65f3f898bd2839233b1b186db92d36a592ec5
선채훈 <80810317+sunchaehoon@users.noreply.github.com>
2021-10-07 15:11:28 +0900

    Create java142.java

---
+++ java142.java
@@ -0,0 +1,12 @@
+package chap06;
+
+public class Circle2 {
+    int r=10;
+    int h=5;
+    double pi = 3.14;
+
+    public double findArea() {
+        double area = r*r*pi;
+        return area;
+    }
+}
