7aed569a7b45cde818c142f2dc857b1614281bf9
선채훈 <80810317+sunchaehoon@users.noreply.github.com>
2021-10-08 11:41:01 +0900

    Create java154.java

---
+++ java154.java
@@ -0,0 +1,10 @@
+package chap06;
+
+public class One1 extends One{
+     void print() {
+//        System.out.println(secret);   // private 불가능
+         System.out.println(roommate);
+         System.out.println(child);
+         System.out.println(anybody);
+    }
+}
