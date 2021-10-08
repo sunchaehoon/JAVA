9aa847d254840d5069e91e5ae1438f7238e8caae
선채훈 <80810317+sunchaehoon@users.noreply.github.com>
2021-10-08 11:42:59 +0900

    Create java157.java

---
+++ java157.java
@@ -0,0 +1,12 @@
+package Test;
+
+import chap06.One;
+
+public class One2 extends One {
+    void print() {
+//        System.out.println(secret);   // private 불가능
+//        System.out.println(roommate);   // default 불가능
+        System.out.println(child);
+        System.out.println(anybody);
+    }
+}
