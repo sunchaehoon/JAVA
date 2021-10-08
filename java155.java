a1de6ef7b97f4dde8f601be373b6c7ac0446fe68
선채훈 <80810317+sunchaehoon@users.noreply.github.com>
2021-10-08 11:41:19 +0900

    Create java155.java

---
+++ java155.java
@@ -0,0 +1,11 @@
+package chap06;
+
+public class Two {
+    void print() {
+        One o = new One();
+//        System.out.println(o.secret);   // private
+        System.out.println(o.roommate);
+        System.out.println(o.child);
+        System.out.println(o.anybody);
+    }
+}
