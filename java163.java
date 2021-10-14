0bc821281f0b49282a883c1524e2c519c61cec3a
선채훈 <80810317+sunchaehoon@users.noreply.github.com>
2021-10-14 14:23:32 +0900

    Create java163.java

---
+++ java163.java
@@ -0,0 +1,12 @@
+package sangsi;
+
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        System.out.println("합계 : " + (a + b));
+    }
+}
